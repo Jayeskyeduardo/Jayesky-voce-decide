cccb199f4b47d6991b8525298e4e57490532ff98
JayeskyEduardo <164092285+Jayeskyeduardo@users.noreply.github.com>
2024-05-29 22:50:47 +0000

    29/05/24

---
+++ script.js
@@ -0,0 +1,49 @@
+const caixaPrincipal = document.qwerySelector(".caixa-principal");
+const caixaPerguntas = document.qwerySelector(".caixa-perguntas");
+const caixaAlternativas = document.qwerySelector(".caixa-alternativas");
+const caixaResultado = document.qwerySelector(".caixa-resultado");
+const textoResultado = document.qwerySelector(".texto-resultado");
+
+const perguntas =[
+  {
+    enunciado: "Pergunta 1",
+    alternativas: [
+      "Respota 1",
+      "Reposta 2"
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 2",
+    alternativas: [
+      "Respota 1",
+      "Reposta 2"
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 3",
+    alternativas: [
+      "Respota 1",
+      "Reposta 2"
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 4",
+    alternativas: [
+      "Respota 1",
+      "Reposta 2"
+    ]
+  },
+
+  {
+    enunciado: "Pergunta 5",
+    alternativas: [
+      "Respota 1",
+      "Reposta 2"
+    ]
+  }
+
+  
+];
